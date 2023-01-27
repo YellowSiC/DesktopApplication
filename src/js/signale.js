d347fdaa8836f9ac024d6ce9df7ceb0411799037
AriBermeki <ari.bermeki.de@gmail.com>
2023-01-27 17:56:51 +0100

    Die Vierte  Commit

--- src/js/signale.js
+++ src/js/signale.js
@@ -1,45 +1,19 @@
-const { app, BrowserWindow, Menu, ipcMain } = require('electron');
-const path = require('path');
+var ws = new WebSocket("ws://localhost:8000/ws");
+ws.onmessage = function(event) {
+    var messages = document.getElementById('messages')
+    var message = document.createElement('li')
+    var content = document.createTextNode(event.data)
+    message.appendChild(content)
+    messages.appendChild(message)
+};
 
 
-
-
-
-  /* let menu = Menu.buildFromTemplate(menuTemplate);
-  Menu.setApplicationMenu(menu);
-
-  ipcMain.on('update-menu', (event, page) => {
-    let menuTemplate = [
-      {
-        label: 'File',
-        submenu: [
-          {
-            label: 'Open Page 1',
-            enabled: page !== 'page1',
-            click() {
-              mainWindow.loadURL('src/pages/index.html');
-            }
-          },
-          {
-            label: 'Open Page 2',
-            enabled: page !== 'page2',
-            click() {
-              mainWindow.loadURL('src/pages/main.html');
-            }
-          },
-          {
-            label: 'Quit',
-            click() {
-              app.quit();
-            }
-          }
-        ]
-      }
-    ];
-
-    menu = Menu.buildFromTemplate(menuTemplate);
-    Menu.setApplicationMenu(menu);
-  }); */
+function sendMessage(event) {
+    var input = document.getElementById("messageText")
+    ws.send(input.value)
+    input.value = ''
+    event.preventDefault()
+}
 
 
 
